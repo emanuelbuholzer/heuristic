7d84c0d856cfb8d5e641b5e2a094b15db7cf899d
Emanuel Buholzer <emanuel0xb@gmail.com>
2023-09-20 22:59:20 +0200

    heuristic bin is now vim-heuristic with some improvements

--- install.sh
+++ install.sh
@@ -1,54 +1,52 @@
 #!/usr/bin/env bash
+
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-echo "installing dotfiles from $script_dir"
+cd "$script_dir"
+
+echo "installing heuristic desktop from $script_dir"
 read -p "do you want to proceed: [y/N] " do_install
 if [ "$do_install" != "y" ]; then
   echo "aborting"
   exit 1
 fi
 
-echo "$script_dir" > ~/.heuristic
 
+echo -e "\n==> checking for updates"
+sudo dnf update
+read -p "do you need to reboot: [y/N] " do_reboot
+if [ "$do_reboot" != "y" ]; then
+  sudo poweroff --reboot
+fi
+
+
+# goodies
+echo "$script_dir" > ~/.heuristic
 xrandr -s 1920x1080
 
-sudo dnf remove \
-  firefox
 
-sudo dnf install \
+# basic packages
+echo -e "\n==> installing basic packages"
+sudo dnf install -y \
   git \
   vim \
   kitty \
   flatpak \
   rofi
 
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak install -y flathub org.gnome.Firmware
-flatpak install -y flathub com.google.Chrome
-# flatpak install -y flathub org.libreoffice.LibreOffice
-# flatpak install -y flathub org.mozilla.Thunderbird
-# flatpak install -y flathub io.dbeaver.DBeaverCommunity
-# flatpak install -y flathub org.filezillaproject.Filezilla
-# flatpak install -y flathub org.gnome.font-viewer
-# flatpak install -y flathub cc.arduino.IDE2
-# flatpak install -y flathub com.jetbrains.IntelliJ-IDEA-Ultimate
-# flatpak install -y flathub org.wireshark.Wireshark
-# flatpak install -y flathub com.jetbrains.CLion
-# flatpak install -y flathub com.github.tchx84.Flatseal
-# flatpak install -y flathub com.usebottles.bottles
-# flatpak install -y flathub us.zoom.Zoom
-# flatpak install -y flathub org.gimp.GIMP
-# flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
-
+# 1password
+echo -e "\n==> installing 1password"
 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-sudo dnf check-update -y 1password-cli 1password && sudo dnf install 1password-cli 1password
+sudo dnf check-update -y 1password && sudo dnf install -y 1password
 cat <<EOF > ~/.ssh/config
 Host *
 	IdentityAgent ~/.1password/agent.sock
 EOF
 
 
+# dotfiles
+echo -e "\n==> installing dotfiles"
 for dotfile in $(find $(pwd) -name ".*" -not -name ".gitignore" -not -name ".git" -not -name ".*.swp"); do
   filename=$(basename $dotfile)
   echo "installing dotfile $filename"
@@ -56,7 +54,39 @@ for dotfile in $(find $(pwd) -name ".*" -not -name ".gitignore" -not -name ".git
 done
 unset dotfile
 
-echo "installing fonts"
+
+# fira code
+echo -e "\n==> installing fonts"
 mkdir -p "$HOME/.local/share"
 ln -snf "$script_dir/fonts" "$HOME/.local/share/fonts"
 fc-cache -rf
+
+
+# setup flatpak and some basic apps
+echo -e "\n==> installing flatpak and basic apps"
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y flathub org.gnome.Firmware
+flatpak install -y flathub com.google.Chrome
+
+flatpak install -y flathub org.gnome.font-viewer
+flatpak install -y flathub com.github.tchx84.Flatseal
+flatpak install -y flathub us.zoom.Zoom
+flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
+
+flatpak install -y flathub org.wireshark.Wireshark
+flatpak install -y flathub org.libreoffice.LibreOffice
+# flatpak install -y flathub com.jetbrains.CLion
+# flatpak install -y flathub com.jetbrains.IntelliJ-IDEA-Ultimate
+# flatpak install -y flathub org.mozilla.Thunderbird
+# flatpak install -y flathub io.dbeaver.DBeaverCommunity
+# flatpak install -y flathub org.filezillaproject.Filezilla
+# flatpak install -y flathub cc.arduino.IDE2
+# flatpak install -y flathub com.usebottles.bottles
+# flatpak install -y flathub org.gimp.GIMP
+
+# cleanup
+# TODO: home directories
+echo -e "\n==> cleaning up"
+sudo dnf remove -y \
+  firefox
