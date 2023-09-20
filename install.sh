4120ee0fab549c56187c22ea90edd5c3f2aced19
Emanuel Buholzer <emanuel0xb@gmail.com>
2023-09-20 16:23:42 +0200

    1Password cleanup

--- install.sh
+++ install.sh
@@ -15,6 +15,10 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
 sudo dnf check-update -y 1password-cli 1password && sudo dnf install 1password-cli 1password
+cat <<EOF > ~/.ssh/config
+Host *
+	IdentityAgent ~/.1password/agent.sock
+EOF
 
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 echo "installing dotfiles from $script_dir"
