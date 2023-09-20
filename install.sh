1caa0844426c6614e0525f95e57daac7b9ad598e
Emanuel Buholzer <emanuel0xb@gmail.com>
2023-09-20 16:17:21 +0200

    1password and some i3 configs

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ sudo dnf install \
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
+sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+sudo dnf check-update -y 1password-cli 1password && sudo dnf install 1password-cli 1password
+
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 echo "installing dotfiles from $script_dir"
 read -p "do you want to proceed: [y/N] " do_install
