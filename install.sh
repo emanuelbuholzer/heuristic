0a2421e2254346ce23463aa7850752805915e5a2
Emanuel Buholzer <emanuel0xb@gmail.com>
2023-09-26 01:09:08 +0200

    ensure .ssh exists, flatpak user install and add distrobox

--- install.sh
+++ install.sh
@@ -46,7 +46,8 @@ sudo dnf install -y \
   rofi \
   arandr \
   virt-viewer \
-  virt-manager
+  virt-manager \
+  distrobox
 
 
 # 1password
@@ -54,6 +55,7 @@ echo -e "\n==> installing 1password"
 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
 sudo dnf check-update -y 1password-cli 1password && sudo dnf install -y 1password-cli 1password
+mkdir ~/.ssh
 cat <<EOF > ~/.ssh/config
 Host *
 	IdentityAgent ~/.1password/agent.sock
@@ -104,22 +106,22 @@ fc-cache -rf
 
 # setup flatpak and some basic apps
 echo -e "\n==> installing flatpak and basic apps"
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak install -y flathub org.gnome.Firmware
-flatpak install -y flathub com.google.Chrome
+flatpak install --user -y flathub org.gnome.Firmware
+flatpak install --user -y flathub com.google.Chrome
 
-flatpak install -y flathub org.gnome.font-viewer
-flatpak install -y flathub com.github.tchx84.Flatseal
-# for the gtk theming
-flatpak install -y flathub com.github.themix_project.Oomox
-flatpak install -y flathub us.zoom.Zoom
-flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
+flatpak install --user -y flathub org.gnome.font-viewer
+flatpak install --user -y flathub com.github.tchx84.Flatseal
+# for the gtk th--user eming
+flatpak install --user -y flathub com.github.themix_project.Oomox
+flatpak install --user -y flathub us.zoom.Zoom
+flatpak install --user -y flathub com.github.IsmaelMartinez.teams_for_linux
 
-flatpak install -y flathub org.wireshark.Wireshark
-flatpak install -y flathub org.libreoffice.LibreOffice
-flatpak install -y flathub io.dbeaver.DBeaverCommunity
-flatpak install -y flathub org.remmina.Remmina
+flatpak install --user -y flathub org.wireshark.Wireshark
+flatpak install --user -y flathub org.libreoffice.LibreOffice
+flatpak install --user -y flathub io.dbeaver.DBeaverCommunity
+flatpak install --user -y flathub org.remmina.Remmina
 # flatpak install -y flathub org.filezillaproject.Filezilla
 # flatpak install -y flathub com.usebottles.bottles
 # flatpak install -y flathub org.gimp.GIMP
